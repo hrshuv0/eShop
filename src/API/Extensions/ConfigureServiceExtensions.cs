06731f54f89974a2219ea3bd371d511d3e057f4a
hrshuv0 <h.r.shuvo9@gmail.com>
2023-08-22 15:31:54 +0600

    add api validation

---
+++ src/API/Extensions/ConfigureServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ConfigureServiceExtensions
+{
+    public static void AddConfigureServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = actionContext =>
+            {
+                var errors = actionContext.ModelState
+                    .Where(e => e.Value!.Errors.Count > 0)
+                    .SelectMany(x => x.Value!.Errors)
+                    .Select(x => x.ErrorMessage).ToArray();
+
+                var errorResponse = new ApiValidationErrorResponse
+                {
+                    Errors = errors
+                };
+
+                return new BadRequestObjectResult(errorResponse);
+            };
+        });
+        
+    }
+
+}
--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,8 +1,10 @@
+using API.Errors;
 using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +14,18 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
 
-
+#region Dependency Injection
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
 
+#endregion
+
+#region Extensiions
 await builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddConfigureServices(builder.Configuration);
+
+#endregion
 
 
 var app = builder.Build();
