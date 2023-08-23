a165f3022be8d972a0b01948940346b4eeeafbf2
hrshuv0 <h.r.shuvo9@gmail.com>
2023-08-23 17:42:16 +0600

    SPA - search

--- src/client/src/app/shop/shop.component.ts
+++ src/client/src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { ShopService } from "./shop.service";
 import { IProduct } from "../_models/product";
 import { IBrand } from "../_models/brand";
@@ -11,6 +11,7 @@ import { ShopParams } from "../_models/shop-params";
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit{
+  @ViewChild('search', {static:true}) searchTerm: ElementRef;
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
@@ -77,6 +78,17 @@ export class ShopComponent implements OnInit{
 
   onPageChanged(event: any){
     this.shopParams.pageNumber = event;
+    this.getProducts();
+  }
+
+  onSearch(){
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.getProducts();
+  }
+
+  onReset(){
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
     this.getProducts();
   }
 
