e92394107dabecd9f011eda3a6805709c57294af
hrshuv0 <h.r.shuvo9@gmail.com>
2023-08-24 09:55:19 +0600

    SPA - add toastr notification

--- src/client/src/app/core/interceptors/error.interceptor.ts
+++ src/client/src/app/core/interceptors/error.interceptor.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import {catchError, Observable, throwError} from 'rxjs';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private toastr: ToastrService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError(error => {
         if(error){
+          if(error.status === 400){
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
+          if(error.status === 401){
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
           if(error.status === 404){
-            this.router.navigateByUrl('not-found');
+            this.router.navigateByUrl('not-found').then(r => r);
           }
           if(error.status === 500){
-            this.router.navigateByUrl('server-error');
+            this.router.navigateByUrl('server-error').then(r => r);
           }
         }
         return throwError(error);
