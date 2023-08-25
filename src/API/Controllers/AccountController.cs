ebff67872262f6c2198bf7558395f05ec5d504d6
hrshuv0 <h.r.shuvo9@gmail.com>
2023-08-25 23:29:54 +0600

    add load user, address, update address

--- src/API/Controllers/AccountController.cs
+++ src/API/Controllers/AccountController.cs
@@ -1,7 +1,11 @@
+using System.Security.Claims;
 using API.Dtos;
 using API.Errors;
+using API.Extensions;
+using AutoMapper;
 using Core.Entities.Identity;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,12 +16,23 @@ public class AccountController : BaseApiController
     private readonly UserManager<AppUser> _userManager;
     private readonly SignInManager<AppUser> _signInManager;
     private readonly ITokenService _tokenService;
+    private IMapper _mapper;
 
-    public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService)
+    public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService, IMapper mapper)
     {
         _userManager = userManager;
         _signInManager = signInManager;
         _tokenService = tokenService;
+        _mapper = mapper;
+    }
+    
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+        var user = await _userManager.FindByEmailFromClaimsPrincipal(HttpContext.User);
+
+        return CreateUserDto(user);
     }
     
     [HttpPost("login")]
@@ -40,10 +55,10 @@ public class AccountController : BaseApiController
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
-        if (await CheckEmailExistsAsync(registerDto.Email))
-        {
-            return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new []{"Email address is in use"}});
-        }
+        // if (await CheckEmailExistsAsync(registerDto.Email))
+        // {
+        //     return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new []{"Email address is in use"}});
+        // }
 
         var user = new AppUser
         {
@@ -59,15 +74,44 @@ public class AccountController : BaseApiController
         return CreateUserDto(user);
     }
     
-    
-    
-    
-
-    private async Task<bool> CheckEmailExistsAsync(string registerDtoEmail)
+    [HttpGet]
+    [Route("emailexists")]
+    public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
     {
-        return await _userManager.FindByEmailAsync(registerDtoEmail)
-            .ContinueWith(task => task.Result != null);
+        return await _userManager.FindByEmailAsync(email) != null;
     }
+
+    // private async Task<bool> CheckEmailExistsAsync(string registerDtoEmail)
+    // {
+    //     return await _userManager.FindByEmailAsync(registerDtoEmail)
+    //         .ContinueWith(task => task.Result != null);
+    // }
+    
+    [Authorize]
+    [HttpGet("address")]
+    public async Task<IActionResult> GetUserAddress()
+    {
+        var user = await _userManager.FindUserByClaimsPrincipleWithAddressAsync(User);
+        var address = _mapper.Map<AddressDto>(user.Address);
+        
+        return Ok(address);
+    }
+    
+    [Authorize]
+    [HttpPut("address")]
+    public async Task<IActionResult> UpdateUserAddress(AddressDto address)
+    {
+        var user = await _userManager.FindUserByClaimsPrincipleWithAddressAsync(User);
+        user.Address = _mapper.Map<AddressDto, Address>(address);
+
+        var result = await _userManager.UpdateAsync(user);
+
+        if (result.Succeeded) 
+            return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+
+        return BadRequest("Problem updating the user");
+    }
+    
 
     private ActionResult<UserDto> CreateUserDto(AppUser user)
     {
--- src/API/Helpers/MappingProfiles.cs
+++ src/API/Helpers/MappingProfiles.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities.Identity;
 
 namespace API.Helpers;
 
@@ -6,10 +8,12 @@ public class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
-        CreateMap<Core.Entities.Product, Dtos.ProductToReturnDto>()
+        CreateMap<Core.Entities.Product, ProductToReturnDto>()
             .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand!.Name))
             .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType!.Name))
             .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+        
+        CreateMap<Address, AddressDto>().ReverseMap();
     }
     
 }
