a98adbe28cc8d682c609cedb9093b4d4acb00dde
hrshuv0 <h.r.shuvo9@gmail.com>
2023-08-27 17:50:54 +0600

    SPA - add checkout sub components

--- src/client/src/app/checkout/checkout.module.ts
+++ src/client/src/app/checkout/checkout.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout.component';
 import { CheckoutRoutingModule } from "./checkout-routing.module";
 import { SharedModule } from "../shared/shared.module";
+import { CheckoutAddressComponent } from './checkout-address/checkout-address.component';
+import { CheckoutReviewComponent } from './checkout-review/checkout-review.component';
+import { CheckoutDeliveryComponent } from './checkout-delivery/checkout-delivery.component';
+import { CheckoutPaymentComponent } from './checkout-payment/checkout-payment.component';
 
 
 
 @NgModule({
   declarations: [
-    CheckoutComponent
+    CheckoutComponent,
+    CheckoutAddressComponent,
+    CheckoutReviewComponent,
+    CheckoutDeliveryComponent,
+    CheckoutPaymentComponent
   ],
   imports: [
     CommonModule,
--- src/client/src/app/shared/components/stepper/stepper.component.ts
+++ src/client/src/app/shared/components/stepper/stepper.component.ts
@@ -16,7 +16,6 @@ export class StepperComponent extends CdkStepper implements OnInit{
 
   onClick(index: number): void {
     this.selectedIndex = index;
-    console.log(this.selectedIndex);
   }
 
 }
