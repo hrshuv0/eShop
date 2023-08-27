7c519616d11bb28ca5c49d0c1d039daaea1ef924
hrshuv0 <h.r.shuvo9@gmail.com>
2023-08-27 17:11:49 +0600

    map picture-url for order item

--- src/API/Helpers/MappingProfiles.cs
+++ src/API/Helpers/MappingProfiles.cs
@@ -30,7 +30,8 @@ public class MappingProfiles : Profile
         CreateMap<OrderItem, OrderItemDto>()
             .ForMember(d => d.ProductId, o => o.MapFrom(s => s.ItemOrdered!.ProductItemId))
             .ForMember(d => d.ProductName, o => o.MapFrom(s => s.ItemOrdered!.ProductName))
-            .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.ItemOrdered!.PictureUrl));
+            .ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.ItemOrdered!.PictureUrl))
+            .ForMember(d => d.PictureUrl, o => o.MapFrom<OrderItemUrlResolver>());
     }
     
 }
